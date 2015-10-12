19a64c974ca1be48eeb78e9c25b937a7593ccf17
anthonybrown <tony@tonybrown.me>
2015-10-12 18:25:12 -0400

    fixed typo

--- server/lib/initDB.js
+++ server/lib/initDB.js
@@ -1,1 +1,4 @@
-  adminUser: { email: 'tony@tonybrown.me', password: 'JesusInMe', admin: true, firstName: 'Tony', lastName: 'Brown' },
+ar initDB = {
+	  adminUser: { email: 'tony@tonybrown.me', password: 'JesusInMe', admin: true, firstName: 'Tony', lastName: 'Brown'  },
+
+});
